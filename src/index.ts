3403d50966b3104a35b90d0eadce7f49629daafc
Mohamed Fall <ps.hackmaster@gmail.com>
2020-11-22 04:15:12 +0100

    init post branch

--- src/index.ts
+++ src/index.ts
@@ -25,9 +25,9 @@ app.set("views", path.join(__dirname, 'views'))
 
 app.use(cors({credentials: true, origin: [/\.gripurl\.com$/]})); 
 app.use(cookieParser());
-app.use(forcessl());
 app.use(express.json());
 app.use("/static",express.static(path.join(__dirname, 'static')))
+// app.use(forcessl()); Only In Prod
 
 app.use('/api/v1/urls', urlRoutes)
 app.use('/api/v1/auth', authRoutes);
